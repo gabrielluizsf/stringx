2f6b48320babb45024c8752e0bb203a84e168392
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-06-10 10:25:23 -0300

    feat: add HasPrefix method
    This commit introduces the `HasPrefix` method to the `stringx` package's `String` type.
    
    The `HasPrefix` method checks if a given `String` starts with the specified prefix. It returns `true` if the `String` has the prefix and `false` otherwise. This provides a convenient way to determine if a string begins with a particular substring.

---
+++ has_prefix.go
@@ -0,0 +1,6 @@
+package stringx
+
+// HasPrefix checks if the string starts with the specified prefix.
+func (s String) HasPrefix(prefix string) bool {
+	return s.Length() >= len(prefix) && s.String()[:len(prefix)] == prefix
+}

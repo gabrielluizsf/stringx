b1876f13e3db5f9299ccc5b40dc862cedd7d2aef
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-06-26 16:50:24 -0300

    refactor: simplify TestSplit by reusing String instance for assertions

--- split_test.go
+++ split_test.go
@@ -7,9 +7,10 @@ import (
 )
 
 func TestSplit(t *testing.T) {
-	assert.Equal(t, String("a, b, c").Split(", "), []string{"a", "b", "c"})
-	assert.Equal(t, String("a, b, c").Split("; "), []string{"a, b, c"})
-	assert.Equal(t, len(String("a, b, c").Split(Empty.String())), 7)
+	s := String("a, b, c")
+	assert.Equal(t, s.Split(", "), []string{"a", "b", "c"})
+	assert.Equal(t, s.Split("; "), []string{"a, b, c"})
+	assert.Equal(t, len(s.Split(Empty.String())), 7)
 	result := String("github.com/i9si/atendi9api/internal/server/handler/whatsapp.processLicensePlate").Split(".")
 	expected := []string{"github", "com/i9si/atendi9api/internal/server/handler/whatsapp", "processLicensePlate"}
 	assert.Equal(t, result, expected)

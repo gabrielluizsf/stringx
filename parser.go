891d343384776cc65b2a9fcab7c5d0ac989e7bfd
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-07-03 11:37:13 -0300

    feat: implement string parser with boolean, integer, and float parsing
    This commit introduces a new `Parser` struct and associated methods for parsing strings into boolean, integer, and float values.
    
    The `Parser` struct provides the following functionalities:
    
    - `Bool()`: Parses a string into a boolean value, supporting boolean literals and comparison expressions (e.g., "a == b", "1 > 2").
    - `Int()`: Parses a string into an integer value, supporting integer literals and basic arithmetic operations (+, -, *, /).
    - `Float()`: Parses a string into a float value, supporting float literals and basic arithmetic operations (+, -, *, /).
    
    The implementation includes error handling for invalid expressions and uses the `strconv` package for parsing. It also defines a custom `MathError` type for representing math-related errors.

---
+++ parser.go
@@ -0,0 +1,199 @@
+package stringx
+
+import (
+	"strconv"
+)
+
+// Parser is a string parser.
+type Parser struct {
+	s       String
+	bitSize int
+}
+
+// NewParser creates a [github.com/i9si-sistemas/stringx.Parser].
+func NewParser(s string) *Parser {
+	return &Parser{String(s), 64}
+}
+
+// Bool parses a bool-comparison or a plain bool literal.
+func (p *Parser) Bool() (bool, error) {
+	s := p.s
+	type cmpOp struct {
+		token   string
+		compare func(a, b string) bool
+	}
+	compare := func(
+		a, b string,
+		compareN func(a, b float64) bool,
+		compareS func(a, b string) bool,
+	) bool {
+		aFloat, aErr := NewParser(a).Float()
+		bFloat, bErr := NewParser(b).Float()
+		if aErr == nil && bErr == nil {
+			return compareN(aFloat, bFloat)
+		}
+		return compareS(a, b)
+	}
+
+	ops := []cmpOp{
+		{"!=", func(a, b string) bool { return a != b }},
+		{"==", func(a, b string) bool { return a == b }},
+		{">", func(a, b string) bool {
+			return compare(
+				a, b,
+				func(a, b float64) bool {
+					return a > b
+				},
+				func(a, b string) bool {
+					return a > b
+				},
+			)
+		}},
+		{"<", func(a, b string) bool {
+			return compare(
+				a, b,
+				func(a, b float64) bool {
+					return a < b
+				},
+				func(a, b string) bool {
+					return a < b
+				},
+			)
+		}},
+	}
+
+	for _, op := range ops {
+		if s.Includes(op.token) {
+			parts := s.SplitN(op.token, 2)
+			if len(parts) != 2 {
+				return false, ErrInvalidExpression
+			}
+			clean := func(x String) string {
+				return x.Trim(Space.String()).Trim(`"`).Trim("'").String()
+			}
+			left, right := clean(String(parts[0])), clean(String(parts[1]))
+			return op.compare(left, right), nil
+		}
+	}
+	return strconv.ParseBool(s.String())
+}
+
+// Int parses an integer.
+func (p *Parser) Int() (n int64, err error) {
+	s := p.s
+	parseInt := func(s string) (int64, error) {
+		return strconv.ParseInt(s, 10, p.bitSize)
+	}
+	ops := []mathOp[int64]{
+		{"+", func(a, b string) (int64, error) {
+			return execute(a, b, parseInt, func(a, b int64) (int64, error) {
+				return a + b, nil
+			})
+		}},
+		{"-", func(a, b string) (int64, error) {
+			return execute(a, b, parseInt, func(a, b int64) (int64, error) {
+				return a - b, nil
+			})
+		}},
+		{"*", func(a, b string) (int64, error) {
+			return execute(a, b, parseInt, func(a, b int64) (int64, error) {
+				return a * b, nil
+			})
+		}},
+		{"/", func(a, b string) (int64, error) {
+			return execute(a, b, parseInt, func(a, b int64) (int64, error) {
+				return a / b, nil
+			})
+		}},
+	}
+	for _, op := range ops {
+		if v, err := op.run(s); err == nil {
+			return v, nil
+		}
+	}
+	return parseInt(p.s.String())
+}
+
+// Float parses a float.
+func (p *Parser) Float() (n float64, err error) {
+	s := p.s
+	parseFloat := func(s string) (float64, error) {
+		return strconv.ParseFloat(s, p.bitSize)
+	}
+	ops := []mathOp[float64]{
+		{"+", func(a, b string) (float64, error) {
+			return execute(a, b, parseFloat, func(a, b float64) (float64, error) {
+				return a + b, nil
+			})
+		}},
+		{"-", func(a, b string) (float64, error) {
+			return execute(a, b, parseFloat, func(a, b float64) (float64, error) {
+				return a - b, nil
+			})
+		}},
+		{"*", func(a, b string) (float64, error) {
+			return execute(a, b, parseFloat, func(a, b float64) (float64, error) {
+				return a * b, nil
+			})
+		}},
+		{"/", func(a, b string) (float64, error) {
+			return execute(a, b, parseFloat, func(a, b float64) (float64, error) {
+				return a / b, nil
+			})
+		}},
+	}
+	for _, op := range ops {
+		if v, err := op.run(s); err == nil {
+			return v, nil
+		}
+	}
+	return parseFloat(p.s.String())
+}
+
+// MathError is a math error.
+type MathError string
+
+func (e MathError) Error() string {
+	return String("Math Error:").Concat(NewLine, String(e)).String()
+}
+
+// ErrInvalidExpression is returned when the expression is invalid.
+var ErrInvalidExpression = MathError("Invalid expression")
+
+type mathOp[T int64 | float64] struct {
+	token   string
+	execute func(a, b string) (T, error)
+}
+
+func (op mathOp[T]) run(s String) (T, error) {
+	if s.Includes(op.token) {
+		v := s.Replace(Space.String(), Empty.String()).Split(op.token)
+		if len(v) != 2 {
+			return 0, ErrInvalidExpression
+		}
+		a, b := v[0], v[1]
+		n, err := op.execute(a, b)
+		if err != nil {
+			return 0, err
+		}
+		return n, nil
+	}
+	return 0, ErrInvalidExpression
+}
+
+func execute[T int64 | float64](
+	a, b string,
+	parse func(v string) (T, error),
+	fn func(a, b T) (T, error),
+) (T, error) {
+	aInt, aErr := parse(a)
+	if aErr != nil {
+		return 0, ErrInvalidExpression
+	}
+	bInt, bErr := parse(b)
+	if bErr != nil {
+		return 0, ErrInvalidExpression
+	}
+	return fn(aInt, bInt)
+
+}
---
+++ parser_test.go
@@ -0,0 +1,57 @@
+package stringx
+
+import (
+	"testing"
+
+	"github.com/i9si-sistemas/assert"
+)
+
+func TestParser(t *testing.T) {
+	mustBool := func(s string) bool {
+		v, err := NewParser(s).Bool()
+		assert.NoError(t, err)
+		return v
+	}
+	mustInt := func(s string) int64 {
+		v, err := NewParser(s).Int()
+		assert.NoError(t, err)
+		return v
+	}
+	mustFloat := func(s string) float64 {
+		v, err := NewParser(s).Float()
+		assert.NoError(t, err)
+		return v
+	}
+
+	t.Run("Booleans", func(t *testing.T) {
+		assert.True(t, mustBool(" '' == ''"))
+		assert.True(t, mustBool(` "" == "" `))
+		assert.True(t, mustBool(" '10' == 10"))
+		assert.True(t, mustBool("11 != 10"))
+		assert.False(t, mustBool("'10' != 10"))
+		assert.True(t, mustBool("10 ==  10"))
+		assert.True(t, mustBool("true"))
+		assert.False(t, mustBool("false"))
+		assert.False(t, mustBool("false == true"))
+		assert.True(t, mustBool("true == true"))
+		assert.False(t, mustBool("'10'> 11"))
+		assert.True(t, mustBool("'a' < 'b'"))
+		assert.True(t, mustBool("10 > 9"))
+		assert.False(t, mustBool("'9' > 10"))
+	})
+	t.Run("Integers", func(t *testing.T) {
+		assert.Equal(t, mustInt("10"), 10)
+		assert.Equal(t, mustInt("   10 + 10"), 20)
+		assert.Equal(t, mustInt("10 * 20"), 200)
+		assert.Equal(t, mustInt("10 - 20"), -10)
+		assert.Equal(t, mustInt("10 / 20    "), 0)
+	})
+
+	t.Run("Floats", func(t *testing.T) {
+		assert.Equal(t, mustFloat("10.0"), 10.0)
+		assert.Equal(t, mustFloat("10 + 10.0"), 20.0)
+		assert.Equal(t, mustFloat("10.0 * 20.0"), 200.0)
+		assert.Equal(t, mustFloat("10.0 - 20.0"), -10.0)
+		assert.Equal(t, mustFloat("10.0 / 20"), 0.5)
+	})
+}
